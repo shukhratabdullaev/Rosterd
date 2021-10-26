a39d3bb785b750194ee5ae4acb1915e0a876a2fb
Shukhrat <52028608+fakefunnyface@users.noreply.github.com>
2021-10-26 12:29:38 +0400

    hamburger menu add

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,23 @@ let swiper = new Swiper(".mySwiper", {
         clickable: true,
     },
 });
+
+
+
+(function () {
+
+    let hamburger = {
+        navToggle: document.querySelector('.nav-toggle'),
+        nav: document.querySelector('nav'),
+
+        doToggle: function (e) {
+            e.preventDefault();
+            this.navToggle.classList.toggle('expanded');
+            this.nav.classList.toggle('expanded');
+        }
+    };
+
+    hamburger.navToggle.addEventListener('click', function (e) { hamburger.doToggle(e); });
+    // hamburger.nav.addEventListener('click', function (e) { hamburger.doToggle(e); });
+
+}());
